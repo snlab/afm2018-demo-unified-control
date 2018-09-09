ea1b6706dff9ef700ca3f986b956d5fb377b9cb3
yutao@yu-T440 <yuta_o@outlook.com>
2018-09-09 17:22:21 +0800

    integrate trident/server.py to main.py

--- kytos-napps/snlab/trident_server/main.py
+++ kytos-napps/snlab/trident_server/main.py
@@ -2,14 +2,34 @@
 
 trident server
 """
+from flask import jsonify, request
 
-from kytos.core import KytosNApp, log
+from kytos.core import KytosNApp, log, rest
+from kytos.core.helpers import listen_to
+
+from pyof.foundation.network_types import Ethernet, EtherType, IPv4
+from pyof.foundation.basic_types import DPID, UBInt16, UBInt32
+from pyof.foundation.basic_types import IPAddress
+from pyof.foundation.network_types import LLDP, Ethernet, EtherType, VLAN
+from pyof.v0x01.common.action import ActionOutput as AO10
+from pyof.v0x01.common.phy_port import Port as Port10
+from pyof.v0x01.controller2switch.flow_mod import FlowMod as FM10
+from pyof.v0x01.controller2switch.flow_mod import FlowModCommand as FMC
+from pyof.v0x01.controller2switch.packet_out import PacketOut as PO10
+from pyof.v0x04.common.action import ActionOutput as AO13
+from pyof.v0x04.common.flow_instructions import InstructionApplyAction
+from pyof.v0x04.common.flow_match import OxmOfbMatchField, OxmTLV, VlanId
+from pyof.v0x04.common.port import PortNo as Port13
+from pyof.v0x04.controller2switch.flow_mod import FlowMod as FM13
+from pyof.v0x04.controller2switch.packet_out import PacketOut as PO13
 
 from napps.snlab.trident_server import settings
-from kytos.core.helpers import listen_to
-from napps.snlab.trident_server.trident import server as S
 
-from gevent import spawn
+# from napps.snlab.trident_server.trident import server as S
+# from gevent import spawn
+
+from napps.snlab.trident_server.trident.tridentlib import TridentServer
+from napps.snlab.trident_server.settings import CONFIG_LARK
 
 class Main(KytosNApp):
     """Main class of snlab/trident_server NApp.
@@ -17,10 +37,10 @@ class Main(KytosNApp):
     This class is the entry point for this napp.
     """
 
-    def run_trident_server(self):
-        log.info('start to run')
-        S.trident.set_ctx_controller(self.controller)
-        S.http_server.serve_forever()
+    # def run_trident_server(self):
+    #     log.info('start to run')
+    #     S.trident.set_ctx_controller(self.controller)
+    #     S.http_server.serve_forever()
 
     def setup(self):
         """Replace the '__init__' method for the KytosNApp subclass.
@@ -33,7 +53,9 @@ class Main(KytosNApp):
         self.nodes = {}
         self.edges = {}
         self.topology_not_set = True
-        log.info('trident server start')
+        self.trident = TridentServer()
+        self.trident.set_ctx_controller(self.controller)
+        log.info('Main setup')
 
     def execute(self):
         """This method is executed right after the setup method execution.
@@ -43,8 +65,62 @@ class Main(KytosNApp):
 
             self.execute_as_loop(30)  # 30-second interval.
         """
-        spawn(self.run_trident_server)
-        log.info('trident server execute')
+        # spawn(self.run_trident_server)
+        log.info('Main execute')
+
+
+    ###########RESTful API##########
+
+    # http://127.0.0.1:8181/api/snlab/trident_server/v1/index
+    @rest('v1/index')
+    def index(self):
+        return "Hello World"
+
+
+    @rest('v1/submit', methods=['POST'])
+    def submit_program(self):
+        trident = self.trident
+        program = request.data
+        try:
+            trident.submit(CONFIG_LARK, program)
+            return 'ok'
+        except Exception as e:
+            raise e
+
+    @rest('v1/ast')
+    def read_program(self):
+        trident = self.trident
+        return trident.ctx.ast.pretty()   
+
+    @rest('v1/tridentkv')
+    def update_kv(self):
+        trident = self.trident
+        pkt = request.args.get('flow')
+        sa_name = request.args.get('key')
+        value = request.args.get('value')
+        trident.update_sa(sa_name, pkt, value)
+        return 'ok'
+
+
+    ##############Packet In###############
+    @listen_to('kytos/of_core.v0x0[14].messages.in.ofpt_packet_in')
+    def packet_in(self, event):
+        from pyof.v0x04.asynchronous.packet_in import PacketIn
+        msg = event.content['message']
+        assert isinstance(msg, PacketIn)
+        eth = Ethernet()
+        eth.unpack(msg.data.value)
+        log.info('ethernet type=%s'%str(eth.ether_type))
+        if eth.ether_type == EtherType.IPV4:
+            ipv4 = IPv4()
+            ipv4.unpack(eth.data.value)
+            log.info(ipv4.source)
+            log.info(ipv4.destination)
+
+            # TODO add filter
+            # TODO: the format of packet
+            # trident.new_pkt(ipv4)    
+
 
     @listen_to('.*.reachable.mac')
     def handle_reachable_mac(self, event):
@@ -74,9 +150,9 @@ class Main(KytosNApp):
             self.set_nodes(topology)
             self.set_edges(topology)
 
-            S.trident.ctx.set_topology(self.nodes, self.edges)
+            self.trident.ctx.set_topology(self.nodes, self.edges)
 
-            S.trident.ctx.test()
+            self.trident.ctx.test()
 
     def set_nodes(self, topology):
         switches = topology.switches
@@ -108,4 +184,4 @@ class Main(KytosNApp):
 
         If you have some cleanup procedure, insert it here.
         """
-        S.http_server.stop()
+        pass
